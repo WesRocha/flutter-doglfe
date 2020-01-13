605987415393d31311401b63fade066252452b6d
WesRocha <39282367+WesRocha@users.noreply.github.com>
2020-01-13 16:46:25 -0300

    second commit

--- lib/pages/reset-password.page.dart
+++ lib/pages/reset-password.page.dart
@@ -3,6 +3,32 @@ import 'package:flutter/material.dart';
 class ResetPasswordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Colors.black38,
+          onPressed: () => Navigator.pop(context, false),
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(
+          top: 60,
+          left: 40,
+          right: 40,
+        ),
+        color: Colors.white,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 200,
+              height: 200,
+              child: Image.asset("assets/reset-passowrd-icon.png"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
